a08fb6e261b70ff32bc14e0db48fcf2489b20123
oluizfcs <santana.luiz@estudantes.ifg.edu.br>
2026-04-08 09:03:53 -0300

    aula1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "example/test"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
