3ee04b2f21d150b59cc5c9810d4330dcb8c8b60f
oluizfcs <luizfelipexilipe@gmail.com>
2026-06-19 11:08:59 -0300

    404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  rescue_from Mongoid::Errors::DocumentNotFound, with: :render_not_found
 
   private
   
@@ -8,5 +9,9 @@ class ApplicationController < ActionController::Base
     unless current_user&.admin?
       redirect_to root_path, alert: "Acesso restrito a administradores."
     end
+  end
+
+  def render_not_found
+    render file: Rails.public_path.join('404.html'), status: :not_found, formats: [:html]
   end
 end
